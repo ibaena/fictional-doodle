389396e0623e3c8cdf68b50ecab958b117eb9b98
Ivan <ivan.baena04@gmail.com>
2017-03-01 22:05:34 -0500

    initialized

---
+++ imports/ui/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+//import Header from '../../ui/components/header/header.jsx';
+
+
+
+// This component - represents the Home Page
+export const Home = () =>
+  <div id="home-page" className="container-fluid">
+      home
+  </div>
